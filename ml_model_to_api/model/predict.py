e17cc667c7e365dd277080400ff415650da0d5c7
Rajat Goswami <rajatgoswami7333@gmail.com>
2026-03-26 11:34:15 +0100

    adding confidence level to model prediction

--- ml_model_to_api/model/predict.py
+++ ml_model_to_api/model/predict.py
@@ -8,7 +8,21 @@ with open('/Users/rajatgoswami/Downloads/fastAPI/ml_model_to_api/model.pkl','rb'
 
 MODEL_VERSION = '1.1.0' #this is a setup model number and this is usually given by a software like MLFlow
 
+#get class labels from model (important for matching probabilities to class names)
+class_labels = model.classes_.tolist()
+
 def predict_output(user_input:dict):
-    input_df = pd.DataFrame([user_input])
-    prediction =  model.predict(input_df)[0] #here we'll receive a list of ouput and we'll require the 0th item of that list
-    return prediction
+    df = pd.DataFrame([user_input])
+
+    predicted_class = model.predict(df)[0]
+
+    probabilies = model.predict_proba(df)[0]
+    confidence = max(probabilies)
+
+    #create mapping : {class_name :probabilities}
+
+    class_probs = dict (zip(class_labels,map(lambda p: round(p,4), probabilies)))
+
+    return {'predicted_category' : predicted_class,
+            'confidence': round(confidence,4),
+            'class_probabilities': class_probs}
