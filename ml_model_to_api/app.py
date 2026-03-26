f00bbf098d18621f03967f8102b5222e36dc55bd
Rajat Goswami <rajatgoswami7333@gmail.com>
2026-03-26 11:25:55 +0100

    adding try except block

--- ml_model_to_api/app.py
+++ ml_model_to_api/app.py
@@ -31,6 +31,10 @@ def predict_premium(data: User_Input): #this tells what kind of data will be rec
                    'income_lpa': data.income_lpa,
                    'occupation': data.occupation}
    
+    try:
+
+        prediction = predict_output(user_input) #here we'll receive a list of ouput and we'll require the 0th item of that list
+        return JSONResponse(status_code=200, content={'predicted_category': prediction})
     
-    prediction = predict_output(user_input) #here we'll receive a list of ouput and we'll require the 0th item of that list
-    return JSONResponse(status_code=200, content={'predicted_category': prediction})
+    except Exception as e:
+        return JSONResponse(status_code=500, content=str(e))
